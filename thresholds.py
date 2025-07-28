53ab83631af1e3a839849ac35131c6dbadcf52c6
royanrosyad85 <royan.rosyad21@mhs.uinjkt.ac.id>
2025-07-28 20:32:27 +0700

    fix: Correct pro mode thresholds for KNEE and PASS angles in beginner mode

--- thresholds.py
+++ thresholds.py
@@ -6,7 +6,7 @@ def get_thresholds_beginner():
     _ANGLE_HIP_KNEE_VERT = {
                             'NORMAL' : (0,  32),
                             'TRANS'  : (35, 65),
-                            'PASS'   : (70, 95)
+                            'PASS'   : (70, 105)
                            }    
 
         
@@ -15,7 +15,7 @@ def get_thresholds_beginner():
 
                     'HIP_THRESH'   : [10, 50],
                     'ANKLE_THRESH' : 45,
-                    'KNEE_THRESH'  : [50, 70, 95],
+                    'KNEE_THRESH'  : [50, 70, 105],
 
                     'OFFSET_THRESH'    : 35.0,
                     'INACTIVE_THRESH'  : 15.0,
